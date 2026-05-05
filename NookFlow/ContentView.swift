033cc874c2dd4b8cddeff100d3eb71f6211f7280
larva <larva@live.cn>
2026-05-05 17:02:00 +0800

    Initial Commit

---
+++ NookFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NookFlow
+//
+//  Created by 嘻咩咩🐑 on 2026/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NookFlow/NookFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NookFlowApp.swift
+//  NookFlow
+//
+//  Created by 嘻咩咩🐑 on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct NookFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
